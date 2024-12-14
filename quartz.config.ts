ea70a052ed9e0c5da3fc3618f83bcbfd3cd4e4fb
Josh <lopezjosh23@gmail.com>
2024-12-14 14:47:05 -0500

    Quartz sync: Dec 14, 2024, 2:47 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -13,7 +13,7 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: "google", tagID: "G-42C5DP5G8M"
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
